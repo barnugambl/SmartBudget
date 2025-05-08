bdfe0d9a5bc2203b2c625aa230495254ba1dd520
barnugambl <barnugambl@list.ru>
2025-05-08 15:29:52 +0300

    add setup category screen

--- SmartBudgetProject/Coordinator/AppCoordinator.swift
+++ SmartBudgetProject/Coordinator/AppCoordinator.swift
@@ -9,49 +9,19 @@ import UIKit
 
 class AppCoordinator: CoordinatorProtocol {
     var navigationController: UINavigationController
-    var finanicalGoalViewModel = FinancialGoalViewModel()
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
     }
         
-    func showMainTabBar() {
-        let tabBarController = CustomTabBarController()
-        
-        
-        let goalsVM = finanicalGoalViewModel
-        
-
-        let goalsVC = UINavigationController(rootViewController: FinancialGoalsViewController(viewModel: goalsVM))
-        let goals = goalsVC.viewControllers[0] as? FinancialGoalsViewController
    
-        
-        goals?.coordinator = self
-        
-        tabBarController.viewControllers = [goalsVC]
-        tabBarController.setupTabBar()
-        navigationController.setViewControllers([tabBarController], animated: true)
-    }
-        
     func start() {
-        showMainTabBar()
+        showSetupCategoryScreen(title: "Mock", iconName: R.image.coins_icon.name, iconColor: .systemOrange)
     }
     
-    func showAddFinancialGoalScreen() {
-        let vc = AddFinancialGoalViewController(viewModel: finanicalGoalViewModel)
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: true)
-    }
     
-        
-    func showFinancialGoalScreen() {
-        let vc = FinancialGoalsViewController(viewModel: finanicalGoalViewModel)
-        vc.coordinator = self
-        navigationController.pushViewController(vc, animated: true)
-    }
-    
-    func showAddMoneyScreen(name: String) {
-        let viewModel = AddMoneyFinancialGoalViewModel()
-        let vc = AddMoneyFinancialGoalViewController(viewModel: viewModel, name: name)
+    func showSetupCategoryScreen(title: String, iconName: String, iconColor: UIColor) {
+        let viewModel = SetupCategoryViewModel()
+        let vc = SetupCategoryViewController(viewModel: viewModel, title: title, iconName: iconName, iconColor: iconColor)
         vc.coordinator = self
         navigationController.pushViewController(vc, animated: true)
     }
